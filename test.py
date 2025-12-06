739fb93cea1017952de5fd0f804a802932a178ab
Filip <targosz.f@gmail.com>
2025-12-06 18:32:07 +0100

    tdas

--- test.py
+++ test.py
@@ -2,9 +2,9 @@ import requests
 
 # wysyłanie zadania
 task = {"name": "Nauka Tkinter", "repeat": "Every x"}
-res = requests.post("http://127.0.0.1:5000/get_tasks", json=task)
+res = requests.post("http://192.168.50.200:5000/get_tasks", json=task)
 print(res.json())
 
 # pobieranie wszystkich zadań
-res = requests.get("http://127.0.0.1:5000/get_tasks")
+res = requests.get("http://192.168.50.200:5000/get_tasks")
 print(res.json())
